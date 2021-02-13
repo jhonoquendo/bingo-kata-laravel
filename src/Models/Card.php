80603e623b1b32b489c270db5815b75da695a1fa
jhonoquendo <32801020+jhonoquendo@users.noreply.github.com>
2021-02-12 21:01:43 -0500

    Nuevamente las pruebas fallan

--- src/Models/Card.php
+++ src/Models/Card.php
@@ -10,6 +10,32 @@ class Card{
     }
 
     public function isValid():bool{
-        return false;
+        return $this->hasValidSize() && $this->respectBoundaries();
+    }
+
+    public function hasValidSize():bool{
+        foreach($this->grid as $column){
+            if(sizeof($column) !== 5 ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function respectBoundaries():bool{
+        return $this->columnHasElementsBetween($this->grid['B'],1,15)
+        && $this->columnHasElementsBetween($this->grid['I'],16,30)
+        && $this->columnHasElementsBetween($this->grid['N'],31,45)
+        && $this->columnHasElementsBetween($this->grid['G'],46,60)
+        && $this->columnHasElementsBetween($this->grid['O'],61,75);
+    }
+
+    public function columnHasElementsBetween($column,$min,$max):bool{
+        foreach($column as $number){
+            if($number < $min || $number > $max){
+                return false;
+            }
+        }
+        return true;
     }
 }
